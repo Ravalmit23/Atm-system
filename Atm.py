34c8318f0bc99bcb44c141caceb756a455d0c2f3
mit raval <hareshraval8140@gmail.com>
2026-06-05 11:14:17 +0530

    add create and update pin methods

--- Atm.py
+++ Atm.py
@@ -11,6 +11,34 @@ class atm:
         PRESS 5:DEPOSIT AMOUNT
         PRESS 6:EXIT
               ''')
+        user_choice=input("enter your choice:")
+        if user_choice=='1':
+            self.create_pin()
+            self.menu()
+        elif user_choice=='2':
+            self.update_pin()
+        elif user_choice=='3':
+            pass
+        elif user_choice=='4':
+            pass
+        elif user_choice=='5':
+            pass
+        else:
+            exit()
+    def create_pin(self):
+        atm_pin=int(input("ENTER THE PIN:"))
+        self.pin=atm_pin
+        print("YOUR PIN CREATED SUCCESSFULLY")
+
+    def update_pin(self):
+        pin1=int(input("ENTER YOUR OLD PIN:"))
+        if pin1==self.atm_pin:
+            pass
+
+        
+        
+
+        
         
 
 a1=atm()
